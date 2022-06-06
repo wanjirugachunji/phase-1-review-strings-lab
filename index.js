f7c3bd4806ed58eb5ea915f5843e735e7015c317
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-07 01:20:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Alan';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
