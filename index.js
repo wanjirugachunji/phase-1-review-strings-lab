2546d9e32dd1d2fc1c2ae8078afe9ff78f346939
wanjirugachunji <wanjirugachunji@gmail.com>
2022-07-18 20:08:57 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const currentUser = 'Alan';
+const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
 
